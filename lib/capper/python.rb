883c6f85f08d05c65ad6944f070cfd5d73160ab6
Benedikt Böhm <bb@xnull.de>
2011-11-14 15:45:20 +0100

    fix symlink order

--- lib/capper/python.rb
+++ lib/capper/python.rb
@@ -1,5 +1,5 @@
 _cset(:python, "python")
 
 after "deploy:finalize_update" do
-  run("ln -nfs #{latest_release}/#{application} #{latest_release}")
+  run("ln -nfs #{latest_release} #{latest_release}/#{application}")
 end
