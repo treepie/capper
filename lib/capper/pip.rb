703befb3dbf70c2d74e699f7c0642410a6a4ced9
Benedikt Böhm <bb@xnull.de>
2011-11-14 14:53:52 +0100

    merge pip and virtualenv recipes

--- lib/capper/pip.rb
+++
@@ -1,7 +0,0 @@
-after "deploy:finalize_update", "pip:install"
-
-namespace :pip do
-  task :install do
-    run("pip install -E #{deploy_to} -r #{latest_release}/requirements.txt")
-  end
-end
--- lib/capper/virtualenv.rb
+++ lib/capper/virtualenv.rb
@@ -4,10 +4,18 @@ set(:python) { "#{bin_path}/python" }
 
 before "deploy:setup", "virtualenv:setup"
 
+after "deploy:finalize_update", "pip:install"
+
 namespace :virtualenv do
   desc "Create virtualenv for Python packages"
   task :setup, :except => {:no_release => true} do
     run("if [ ! -e #{bin_path}/python ]; then " +
         "virtualenv --no-site-packages #{deploy_to}; fi")
+  end
+end
+
+namespace :pip do
+  task :install do
+    run("#{bin_path}/pip install -E #{deploy_to} -r #{latest_release}/requirements.txt")
   end
 end
