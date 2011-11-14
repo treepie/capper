df3021c99ec72ba5f0cceabe4b09f1d00eff0ff9
Benedikt Böhm <bb@xnull.de>
2011-11-14 15:25:55 +0100

    create symlink for pytohn namespace support

--- lib/capper/python.rb
+++ lib/capper/python.rb
@@ -1,1 +1,9 @@
 _cset(:python, "python")
+
+after "deploy:finalize_update", "python:symlink_ns"
+
+namespace :python do
+  task :symlink_ns, :except => { :no_release => true } do
+    run("ln -nfs #{latest_release}/#{application} #{latest_release}")
+  end
+end
