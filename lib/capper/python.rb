e1a18fc5822faa90548ed0e3bbaed3118efbe56c
Benedikt Böhm <bb@xnull.de>
2011-11-14 15:39:19 +0100

    do not shadow python variable with namespace

--- lib/capper/python.rb
+++ lib/capper/python.rb
@@ -1,9 +1,5 @@
 _cset(:python, "python")
 
-after "deploy:finalize_update", "python:symlink_ns"
-
-namespace :python do
-  task :symlink_ns, :except => { :no_release => true } do
-    run("ln -nfs #{latest_release}/#{application} #{latest_release}")
-  end
+after "deploy:finalize_update" do
+  run("ln -nfs #{latest_release}/#{application} #{latest_release}")
 end
