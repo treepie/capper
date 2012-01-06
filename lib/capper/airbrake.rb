8718bd3ccff1b00f351d84550619790fbd1dcb50
Benedikt Böhm <bb@xnull.de>
2012-01-06 13:45:18 +0100

    fix typo in airbrake recipe

--- lib/capper/airbrake.rb
+++ lib/capper/airbrake.rb
@@ -6,9 +6,8 @@ namespace :airbrake do
     rails_env = fetch(:airbrake_env, fetch(:rails_env, "production"))
     local_user = ENV['USER'] || ENV['USERNAME']
     executable = fetch(:rake, 'rake')
-    directory = configuration.current_release
 
-    notify_command = "cd #{directory}; #{executable} RAILS_ENV=#{rails_env} airbrake:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
+    notify_command = "cd #{current_release}; #{executable} RAILS_ENV=#{rails_env} airbrake:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
 
     logger.info "Notifying Airbrake of Deploy (#{notify_command})"
     result = ""
