cdf9fda26ad9a71d6ed0881253ac85dc17773cd1
Benedikt Böhm <bb@xnull.de>
2012-08-03 10:32:08 +0200

    fix method name clonflict

--- lib/capper/bluepill.rb
+++ lib/capper/bluepill.rb
@@ -1,5 +1,5 @@
 set(:bluepill_script) { File.join(bin_path, "bluepill") }
-set(:bluepill_config) { "#{release_path}/bluepill.rb" }
+set(:bluepillrc) { "#{latest_release}/.bluepillrc" }
 
 after "deploy:update_code", "bluepill:setup"
 before "deploy:restart", "bluepill:start"
@@ -10,20 +10,20 @@ namespace :bluepill do
     servers = find_servers
     configs = fetch(:bluepill_configs, {})
 
-    config = configs.select do |name, config|
-      roles = config[:options][:roles]
-      if roles.nil?
-        true
-      else
-        [roles].flatten.select do |r|
-          self.roles[r.to_sym].include?(server)
-        end.any?
-      end
-    end.map do |name, config|
-      "# #{name}\n#{config[:body]}"
-    end.join("\n\n")
+    upload_template(bluepillrc, :mode => "0644") do |server|
+      config = configs.select do |name, config|
+        roles = config[:options][:roles]
+        if roles.nil?
+          true
+        else
+          [roles].flatten.select do |r|
+            self.roles[r.to_sym].include?(server)
+          end.any?
+        end
+      end.map do |name, config|
+        "# #{name}\n#{config[:body]}"
+      end.join("\n\n")
 
-    upload_template(bluepill_config, :mode => "0644") do |server|
       <<-EOS.dedent
         Bluepill.application("#{application}", :base_dir => "#{shared_path}") do |app|
           #{config}
@@ -38,6 +38,6 @@ namespace :bluepill do
 
   desc "Load bluepill configuration and start it"
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{bluepill_script} load bluepill.rb"
+    run "cd #{current_path} && #{bluepill_script} load #{bluepillrc}"
   end
 end
