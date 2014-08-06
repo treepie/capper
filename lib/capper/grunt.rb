1086a4c9d59b6400aa5fabe071c0492308023180
Ningsuhen Waikhom <ningsuhen@gmail.com>
2014-08-07 01:33:49 +0530

    add grunt task generate_index

--- lib/capper/grunt.rb
+++ lib/capper/grunt.rb
@@ -1,4 +1,5 @@
 after 'deploy:update_code', "grunt:build"
+after 'deploy:update_code', "grunt:generate_index"
 
 namespace :grunt do
   desc <<-DESC
@@ -15,6 +16,21 @@ namespace :grunt do
     node_env        = fetch(:node_env,'production')
     prefix = fetch(:use_nave, false) ? "#{fetch(:nave_dir)}/nave.sh use #{fetch(:node_version, 'stable')}" : ''
     run("cd #{latest_release} && NODE_ENV=#{node_env} #{prefix} #{fetch(:grunt_cmd, "node ./node_modules/.bin/grunt")} build")
+  end
+  desc <<-DESC
+    Run the grunt generate task. \
+    
+    If the grunt cmd cannot be found then you can override the grunt_cmd variable to specifiy \
+    which one it should use.
+
+    You can override any of these defaults by setting the variables shown below.
+
+      set :grunt_cmd,      "grunt" # e.g. "/usr/local/bin/grunt"
+  DESC
+  task :generate_index, :roles => :app, :except => { :no_release => true } do
+    node_env        = fetch(:node_env,'production')
+    prefix = fetch(:use_nave, false) ? "#{fetch(:nave_dir)}/nave.sh use #{fetch(:node_version, 'stable')}" : ''
+    run("cd #{latest_release} && NODE_ENV=#{node_env} #{prefix} #{fetch(:grunt_cmd, "node ./node_modules/.bin/grunt")} execute:generate_index")
   end
 end
 
